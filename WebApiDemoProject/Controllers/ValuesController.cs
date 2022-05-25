f1c57bcd50737c05dadac1c7debf9369ced8905e
m.shikha <m.shikha@DESKTOP-UF1HFJJ>
2022-05-25 11:06:13 +0530

    Add project files.

---
+++ WebApiDemoProject/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemoProject.Employee;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiDemoProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Mahesh> Get()
+
+        {
+            List<Mahesh> forecasts = new List<Mahesh>();
+            
+            for (int i = 0; i<0; i++)
+            {
+                forecasts.Add(new Mahesh(i,("mahesh")));
+            }
+            return forecasts;
+
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiDemoProject/Employee/Mahesh.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiDemoProject.Employee
+{
+    public class Mahesh
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Mahesh(int id,string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
